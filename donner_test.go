a97dbc9659ce88f701be37c97b7da41ef6748be2
Julian Fahrer <julian@fahrer.net>
2019-07-04 10:48:46 -0700

    Set strict mode for exec tests

--- donner_test.go
+++ donner_test.go
@@ -7,24 +7,27 @@ import (
 
 func TestExecCommand(t *testing.T) {
 	tests := map[string]struct {
-		config *Cfg
-		params []string
-		expErr error
+		config     *Cfg
+		params     []string
+		expErr     error
+		strictMode bool
 	}{
 		"missing command": {
-			config: &Cfg{Strategies: map[string]Strategy{}},
-			expErr: ErrMissingCommand,
+			config:     &Cfg{Strategies: map[string]Strategy{}},
+			expErr:     ErrMissingCommand,
+			strictMode: true,
 		},
 		"undefined command": {
-			config: &Cfg{Strategies: map[string]Strategy{"test": {Handler: "test"}}, Commands: map[string]Command{"golang": "exec"}},
-			params: []string{"invalid"},
-			expErr: ErrUndefinedCommand,
+			config:     &Cfg{Strategies: map[string]Strategy{"test": {Handler: "test"}}, Commands: map[string]Command{"golang": "exec"}},
+			params:     []string{"invalid"},
+			expErr:     ErrUndefinedCommand,
+			strictMode: true,
 		},
 	}
 
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
-			err := execCommand(test.config, test.params)
+			err := execCommand(test.config, test.params, test.strictMode)
 			assert.EqualError(t, err, test.expErr.Error())
 		})
 	}
